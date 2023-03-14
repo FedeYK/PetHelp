91263a8ebbba693cb11c30b643b907250a762874
fede <frbarca@gmail.com>
2023-03-14 12:55:37 +0100

    adding all

--- application.py
+++ application.py
@@ -256,8 +256,8 @@ for code in default_exceptions:
 
 
 # Use to run local and make tests
-app.run(debug=True, port=8080)
+# app.run(debug=True, port=8080)
 
 # Use to dockerize
-# port = int(os.environ.get('PORT', 5000))
-# app.run(debug=True, host='0.0.0.0', port=port)
+port = int(os.environ.get('PORT', 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
